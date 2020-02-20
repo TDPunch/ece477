e7785a18c532cb1602394a7df8b52e6247189fcb
TDPunch <tylerpunchoh@gmail.com>
2020-02-20 16:19:57 -0500

    Added lab1, lab2, lab3 folders

---
+++ lab2/main.c
@@ -0,0 +1,10 @@
+/* Author: Tyler Punch
+ * Due Date: 02/21/2020
+ * File: main.c
+ * Purpose: This file contains the main function for lab2.
+ */
+
+int main(int argc, char *argv[])
+{
+	
+}
---
+++ lab2/moon_phase.c
@@ -0,0 +1,8 @@
+/* Author: Tyler Punch
+ * Due Date: 02/21/2020
+ * File: moon_phase.c
+ * Purpose: This file contains functions relating finding the phase of the moon from a 
+ * file (Part A). Also the something cool I did (Part B).
+ */
+
+
