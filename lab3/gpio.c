ceb4649864e27ef3dc8208166bd67cab95a3d80a
TDPunch <tylerpunchoh@gmail.com>
2020-02-27 15:23:05 -0500

    Wrote function export_gpio() to enable the GPIO pins I need.

--- lab3/gpio.c
+++ lab3/gpio.c
@@ -1,4 +1,33 @@
 /* Author: Tyler Punch
  * File: gpio.c
- * Purpose: This file contains functions relating to ECE 477 Lab 3
+ * Purpose: This file contains function relating to ECE 477 Lab 3
  */
+
+#include <unistd>
+#include <>
+int export_gpio(char *pin)
+{
+	int fd = 0, err = 0;
+
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Error enabling GPIO\n");
+		return -1;
+	}
+
+	err = write(fd, pin, strlen(pin));
+	if (err < 0) {
+		fprintf(stderr, "Error writing to file\n");
+		return -1;
+	}
+
+	sleep(1);
+	close(fd);
+
+	return 0;
+}
+
+int set_direction()
+{
+
+}
--- lab3/main.c
+++ lab3/main.c
@@ -3,7 +3,15 @@
  * Purpose: Contains main function for testing functions and error checking
  */
 
+#include <stdio.h>
 int main(int argc, char *argv[])
 {
+	char *ptr, *ptr1;
 
+	ptr = "18";
+	ptr1 = "2";
+
+
+	printf("%d\n", sizeof(ptr));
+	printf("%c\n", *(ptr1+1));
 }
